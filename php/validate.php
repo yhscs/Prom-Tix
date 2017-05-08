355feee7b5d683783f07b66537fa7b6d28c44571
CloneTrooper1019 <MPG1019@aol.com>
2017-05-08 11:17:18 -0500

    Context-based sanitization checks
    Names should only have alphabetical characters
    Student ID should only have numerical characters.

--- php/validate.php
+++ php/validate.php
@@ -1,8 +1,8 @@
 <?php
-	function verifySanitization($fieldName, $fieldContext)
+	function verifySanitization($fieldName, $fieldContext, $pattern = "/[^a-zA-Z]+/")
 	{
 		$field = trim($_POST[$fieldName]);
-		$sanitized = preg_replace("/[^a-zA-Z0-9]+/", "", $field);
+		$sanitized = preg_replace($pattern, "", $field);
 		
 		if ($sanitized !== $field)
 			exit('Bad input detected in $fieldContext. Please double check that you inputted this field correctly.');
@@ -15,7 +15,7 @@
 	
 	$first = verifySanitization('first', 'your first name');
 	$last = verifySanitization('last', 'your last name');
-	$id = verifySanitization('id', 'your student id');
+	$id = verifySanitization('id', 'your student id', "/[^0-9]+/");
 	
 	$result=$dbhVote->prepare("SELECT * FROM `electionVoters` WHERE `First Name` = ? AND `Last Name` = ? AND `ID` = ? LIMIT 1");
 	$result->execute(array($first, $last, $id));
