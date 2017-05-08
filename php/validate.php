98644eb47b64273021f7ac59201f54d8c564c3d1
CloneTrooper1019 <MPG1019@aol.com>
2017-05-08 10:47:08 -0500

    More validation, better context for error messages

--- php/validate.php
+++ php/validate.php
@@ -1,18 +1,21 @@
 <?php
-	function verifySanity($fieldName)
+	function verifySanitization($fieldName, $fieldContext)
 	{
 		$field = trim($_POST[$fieldName]);
 		$sanitized = htmlspecialchars($field);
 		
 		if (strpos($sanitized, '&') !== false) // Does this contain any special encoding characters?
-			exit('Illegal input for $fieldName');
+			exit('Illegal input detected in $fieldContext');
+		
+		return $sanitized
 	}
 
 	// Determine if name and ID match
 	require_once('db.php');
-	$first = trim($_POST['first']);
-	$last = trim($_POST['last']);
-	$id = trim($_POST['id']);
+	
+	$first = verifySanitization('first', 'your first name');
+	$last = verifySanitization('last', 'your last name');
+	$id = verifySanitization('id', 'your student id');
 	
 	$result=$dbhVote->prepare("SELECT * FROM `electionVoters` WHERE `First Name` = ? AND `Last Name` = ? AND `ID` = ? LIMIT 1");
 	$result->execute(array($first, $last, $id));
@@ -30,10 +33,10 @@
 			exit('Your ID number was incorrect.');
 	}
 	
-	verifySanity('guestFirst');
-	verifySanity('guestLast');
-	verifySanity('payerFirst');
-	verifySanity('payerLast');
+	verifySanitization('guestFirst','the first name of the guest');
+	verifySanitization('guestLast','the last name of the guest');
+	verifySanitization('payerFirst','the first name of the payer');
+	verifySanitization('payerLast','the last name of the payer');
 
 	exit('OK');
 ?>
